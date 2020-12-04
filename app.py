1098902dce12797c8bcbf10f3f57b1e5fde1ae18
as-chigir <74703306+as-chigir@users.noreply.github.com>
2020-12-04 12:23:05 +0300

    Blank line,<= 50 characters,capitalized,no point
    This body is  imperative, <= 72 characters, explains "what / why / how"

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print("I`m planning to learn git conficts resolution")
+name = input('Please enter your name:')
+surname = input('Please enter your surname:')
+print('Hi, {} {}!'.format(name, surname))
